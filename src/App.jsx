028eeba700af1c47c2c9c8411f47fa199de57aae
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-17 23:17:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,20 @@ import PhoneForm from 'components/PhoneForm/PhoneForm';
 import ContactList from 'components/ContactList/ContactList';
 import FilterContacts from 'components/FilterContacts/FilterContacts';
 import { useGetAllContactsQuery } from 'redux/contact-sliceApi';
+import RegisterForm from 'components/RegisterForm/RegisterForm';
+import LoginForm from 'components/LoginForm/LoginForm';
+import UserMenu from 'components/UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/auth-selectors';
 
 const App = () => {
+  const [count ,setCount] = useState(0)
+
+  const isLoggedIn = useSelector(authSelectors.getIsLOggedIn);
+  console.log("App ~ isLoggedIn", isLoggedIn);
   
-  const { data: contacts = [] } = useGetAllContactsQuery();
+  // const { data: contacts = [] } = useGetAllContactsQuery();
+  // console.log("App ~ contacts", contacts);
 
   const [filter, setFilter] = useState('');
 
@@ -21,7 +31,11 @@ const App = () => {
 
   return (
     <div>
+      
       <Container>
+      <RegisterForm />
+      <LoginForm />
+      <UserMenu />
         <Section title="Phonebook">
           <PhoneForm />
         </Section>
@@ -29,9 +43,10 @@ const App = () => {
         <Section title="Find contacts by name">
           <FilterContacts value={filter} onChangeInput={changeInput} />
         </Section>
-
+          <h1>{count}</h1>
+            <button type='button' onClick={() => setCount(st => st + 1)}>Добавить 1  </button>
         <Section title="Contacts">
-          {contacts.length > 0 && <ContactList filter={filter} />}
+           {isLoggedIn && <ContactList filter={filter} />}
         </Section>
       </Container>
       <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
