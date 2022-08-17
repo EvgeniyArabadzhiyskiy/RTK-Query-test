5b5d202b651815e9b7fc12aba47780e347fe2596
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-17 23:17:48 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import { authApi } from './auth/authSlice';
 import { contactAPI } from './contact-sliceApi';
+import { authSlice } from './auth/auth-slice';
 
-const middleware = getDefaultMiddleware => [...getDefaultMiddleware(), contactAPI.middleware];;
+const middleware = getDefaultMiddleware => [
+  ...getDefaultMiddleware(),
+  // authApi.middleware,
+  contactAPI.middleware,
+];
 
 export const store = configureStore({
   reducer: {
     [contactAPI.reducerPath]: contactAPI.reducer,
+    auth: authSlice.reducer
+    // [authApi.reducerPath]: authApi.reducer,
   },
   middleware,
 });
