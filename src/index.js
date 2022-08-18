e5f8024356b29c0d90401fe0d4df42b44dc25d9f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-18 10:00:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ import { Provider } from "react-redux";
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
       <App />
       </Provider>
       <GlobalStyles />
     </ThemeProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // npm install --save styled-components
