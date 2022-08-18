8def4ba635e380a4643521c72a3be99d5021a29f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-18 10:00:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Container from 'components/Container/Container';
 import PhoneForm from 'components/PhoneForm/PhoneForm';
 import ContactList from 'components/ContactList/ContactList';
 import FilterContacts from 'components/FilterContacts/FilterContacts';
-import { useGetAllContactsQuery } from 'redux/contact-sliceApi';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 import LoginForm from 'components/LoginForm/LoginForm';
 import UserMenu from 'components/UserMenu/UserMenu';
@@ -15,15 +14,10 @@ import { useSelector } from 'react-redux';
 import authSelectors from 'redux/auth/auth-selectors';
 
 const App = () => {
-  const [count ,setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [filter, setFilter] = useState('');
 
   const isLoggedIn = useSelector(authSelectors.getIsLOggedIn);
-  console.log("App ~ isLoggedIn", isLoggedIn);
-  
-  // const { data: contacts = [] } = useGetAllContactsQuery();
-  // console.log("App ~ contacts", contacts);
-
-  const [filter, setFilter] = useState('');
 
   const changeInput = evt => {
     setFilter(evt.target.value);
@@ -31,22 +25,23 @@ const App = () => {
 
   return (
     <div>
-      
       <Container>
-      <RegisterForm />
-      <LoginForm />
-      <UserMenu />
+        <RegisterForm />
+        <LoginForm />
+        <UserMenu />
         <Section title="Phonebook">
-          <PhoneForm />
+        {isLoggedIn && <PhoneForm />}
         </Section>
 
         <Section title="Find contacts by name">
-          <FilterContacts value={filter} onChangeInput={changeInput} />
+        {isLoggedIn && <FilterContacts value={filter} onChangeInput={changeInput} />}
         </Section>
-          <h1>{count}</h1>
-            <button type='button' onClick={() => setCount(st => st + 1)}>Добавить 1  </button>
+        <h1>{count}</h1>
+        <button type="button" onClick={() => setCount(st => st + 1)}>
+          Добавить 1{' '}
+        </button>
         <Section title="Contacts">
-           {isLoggedIn && <ContactList filter={filter} />}
+          {isLoggedIn && <ContactList filter={filter} />}
         </Section>
       </Container>
       <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
